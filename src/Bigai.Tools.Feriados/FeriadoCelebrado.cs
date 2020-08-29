43f0bda7dcedb4f110a490caba3f23b8facd34e0
Marcos Cruz <Marcos Cruz@MARCOS-CRUZ>
2020-08-29 13:41:22 -0300

    Atualização de documentação

--- src/Bigai.Tools.Feriados/FeriadoCelebrado.cs
+++ src/Bigai.Tools.Feriados/FeriadoCelebrado.cs
@@ -71,6 +71,19 @@ namespace Bigai.Tools.Feriados
             FeriadoMovel = feriadoMovel;
         }
 
+        /// <summary>
+        /// Cria uma instância com as informações detalhadas de um feriado.
+        /// </summary>
+        /// <param name="dataFeriado">Data em que ocorre o feriado.</param>
+        /// <param name="descricao">Descrição sobre o que é comemorado.</param>
+        /// <param name="abrangencia">Abrangência do feriado, podendo assumir Nacional, Estadual ou Municipal.</param>
+        /// <param name="pais">Sigla do país onde ocorre o feriado, composta por 2 letras, segundo a norma ISO-3166.</param>
+        /// <param name="estado">Sigla do estado onde ocorre o feriado, composta por 2 letras, segundo a norma ISO-3166.</param>
+        /// <param name="codigoFederalMunicipio">Código Federal que identifica o Município, composto de 1 a 4 digitos numéricos.</param>
+        /// <param name="codigoIbgeMunicipio">Código Ibge que identifica o Município, composto 7 digitos numéricos.</param>
+        /// <param name="pontoFacultativo"><c>true</c> para indicar que é um feriado opcional ou ponto facultativo.</param>
+        /// <param name="feriadoMovel"><c>true</c> para indicar que é um feriado com data móvel.</param>
+        /// <returns>Retorna uma instância com informações detalhadas de um feriado.</returns>
         public static FeriadoCelebrado CriarFeriado(DateTime dataFeriado, string descricao, string abrangencia, string pais, string estado, string codigoFederalMunicipio, string codigoIbgeMunicipio, bool pontoFacultativo, bool feriadoMovel)
         {
             return new FeriadoCelebrado(dataFeriado, descricao, abrangencia, pais, estado, codigoFederalMunicipio, codigoIbgeMunicipio, pontoFacultativo, feriadoMovel);
@@ -80,6 +93,11 @@ namespace Bigai.Tools.Feriados
 
         #region Comparação
 
+        /// <summary>
+        /// Compara se dois objetos são iguais.
+        /// </summary>
+        /// <param name="obj">Instância do objeto que será comparado.</param>
+        /// <returns><c>true</c> se os dois objetos forem iguais, caso contrário <c>false</c>.</returns>
         public override bool Equals(object obj)
         {
             return obj is FeriadoCelebrado celebrado &&
@@ -98,6 +116,10 @@ namespace Bigai.Tools.Feriados
 
         #region Hashcode
 
+        /// <summary>
+        /// Retorna o hash code do objeto.
+        /// </summary>
+        /// <returns></returns>
         public override int GetHashCode()
         {
             HashCode hash = new HashCode();
